015b4eb57800429681366bcc6f06a0c0fb056340
Nguyen Minh Hieu <hieu.nguyen@tiki.vn>
2016-03-16 22:58:23 +0700

    change script

--- deploy/scripts/login.sh
+++ deploy/scripts/login.sh
@@ -4,22 +4,17 @@
 #
 #
 ssh -i identity_files/key.pem $USER@$HOST <<EOF 
-if ! /travel/wwww/travel > /dev/null 2>&1; then
+if [ ! /travel/wwww/travel ]; then
 	echo "dir not found, create new folder";
-	sudo mkdir -p /travel/wwww/travel;
+	sudo mkdir -p /travel/wwww;
 	cd /travel/wwww/;
 	if ! which git > /dev/null 2>&1; then
-    	echo "Nginx not installed"
-    	sudo apt-get install git
-    	#call script install nginx and config nginx
-		#installNginx
-	else
-		sudo git clone git@github.com:plusteams/travel.git travel;
-		cd travel;
-		git checkout dev;
-		sudo deploy/scripts/deploy.sh
+		echo "Nginx not installed"
+		sudo apt-get install git
 	fi
-else
-	echo "s";
+	sudo git clone git@github.com:plusteams/travel.git travel;
 fi
+cd travel;
+sudo git checkout dev;
+sudo deploy/scripts/deploy.sh
 EOF
