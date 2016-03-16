202b37f01aa05e13bd3e67d625267f1f717f2fe7
Nguyen Minh Hieu <hieu.nguyen@tiki.vn>
2016-03-16 21:40:05 +0700

    add login script

---
+++ deploy/scripts/login.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+#
+#
+if ! /travel/wwww/travel > /dev/null 2>&1; then
+	echo "dir not found, create new folder";
+	sudo mkdir -p /travel/wwww/travel;
+	cd /travel/wwww/;
+	if ! which git > /dev/null 2>&1; then
+    	echo "Nginx not installed"
+    	sudo apt-get install git
+    	#call script install nginx and config nginx
+		#installNginx
+	else
+		sudo git clone git@github.com:plusteams/travel.git travel;
+		cd travel;
+		git checkout dev;
+		sudo deploy/scripts/deploy.sh
+	fi
+else
+	echo "s";
+fi
