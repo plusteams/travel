b6d79596f72eaadbd5f79b1c446672e1af13f795
Nguyen Minh Hieu <hieu.nguyen@tiki.vn>
2016-03-27 05:56:36 +0700

    Change repo

--- deploy/scripts/login_prod.sh
+++ deploy/scripts/login_prod.sh
@@ -19,7 +19,7 @@ sudo git checkout master;
 #checkout source code
 #sudo git checkout dev;
 #pull images docker 
-sudo docker pull plusteams/xxxx
+sudo docker pull plusteam/xxxx
 #sudo docker restart $(sudo docker -a -q)
 #sudo git pull;
 #sudo chmod a+x deploy/scripts/deploy.sh
