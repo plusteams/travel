08e16fb0bb6b22a1709d775b2d77b0e98f51ff95
Nguyen Minh Hieu <hieu.nguyen@tiki.vn>
2016-03-27 23:58:38 +0700

    edit deploy

---
+++ deploy/scripts/login_dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#
+#
+#
+ssh -i identity_files/key.pem $USER@$HOST <<EOF 
+if [ /travel/wwww/travel_dev ]; then
+	echo "dir not found, create new folder";
+	sudo mkdir -p /travel/wwww;
+	cd /travel/wwww/;
+	if ! which git > /dev/null 2>&1; then
+		echo "Nginx not installed"
+		sudo apt-get -y install git
+	fi
+	sudo git clone git@github.com:plusteams/travel.git travel_dev;
+fi
+cd travel_dev;
+#checkout source code
+sudo git checkout dev;
+sudo git pull;
+#pull images docker 
+sudo docker pull plusteam/xxxx
+sudo docker restart $(sudo docker ps -a -q)
+#sudo chmod a+x deploy/scripts/deploy.sh
+#sudo deploy/scripts/deploy.sh nginx
+EOF
